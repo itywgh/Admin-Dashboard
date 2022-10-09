66e1eed3728aa1ed65040fce1b19b96eec461713
Roro Bodkins <gitopwtd.xfnfr@slmail.me>
2022-10-09 11:48:04 +0200

    Fix alignment of sidebar elements

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const sidebarReveal = document.querySelector('.sidebar');
 const sidebarDashboardLogo = document.querySelector('.sidebar-dashboard-logo');
 const logoSidebarReveal = document.querySelector('.logo-sidebar');
 const sidebarDashboardPReveal = document.querySelector('.sidebar-dashboard-p');
+const sidebarUlActive = document.querySelector('.list-1-sidebar');
+const sidebarUlActive2 = document.querySelector('.list-2-sidebar');
+const svgReveal = document.querySelectorAll('svg');
+const sidebarLiReveal = document.querySelectorAll('li');
 
 // EVENT LISTENERS
 dashboardButton.addEventListener('click', () => {
@@ -11,6 +15,10 @@ dashboardButton.addEventListener('click', () => {
   logoSidebarReveal.classList.add('logo-sidebar-active');
   sidebarDashboardLogo.classList.add('sidebar-dashboard-logo-active');
   sidebarDashboardPReveal.classList.add('sidebar-dashboard-p-active');
+  sidebarUlActive.classList.add('sidebar-ul-active');
+  sidebarUlActive2.classList.add('sidebar-ul-active');
+  svgReveal.forEach(svg => svg.classList.add('sidebar-svg-active'));
+  sidebarLiReveal.forEach(li => li.classList.add('sidebar-li-active'));
  });
 
 sidebarDashboardLogo.addEventListener('click', () => { 
@@ -18,6 +26,10 @@ sidebarDashboardLogo.addEventListener('click', () => {
   logoSidebarReveal.classList.remove('logo-sidebar-active');
   sidebarDashboardLogo.classList.remove('sidebar-dashboard-logo-active');
   sidebarDashboardPReveal.classList.remove('sidebar-dashboard-p-active');
+  sidebarUlActive.classList.remove('sidebar-ul-active');
+  sidebarUlActive2.classList.remove('sidebar-ul-active');
+  svgReveal.forEach(svg => svg.classList.remove('sidebar-svg-active'));
+  sidebarLiReveal.forEach(li => li.classList.remove('sidebar-li-active'));
 });
 
 
